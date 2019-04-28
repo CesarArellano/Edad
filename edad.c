58c74fe6858837cf21edcd1cb54497c70ad71bc6
RayWayDay <raywayday@gmail.com>
2019-04-28 16:24:32 -0500

    Edad Corregido

--- edad.c
+++ edad.c
@@ -1,45 +1,52 @@
 #include <stdio.h>
 #include <time.h>
 
-void Edad(int d, int m, int a,int d2, int m2, int a2);
+typedef struct def_Nodo
+{
+	int Dia, Mes, Anio, Edad;
+}Date;
+
+int CalcEdad(Date Fecha, int *flag);
 
 int main (void)
 {
-	int Dia, Mes, Anio;
-	int DiaActual, MesActual, AnioActual;
+	Date Fecha; 
+	int flag=0;
+	printf("Ingresa tu dia de nacimiento\n");
+	scanf(" %d", &Fecha.Dia);
+	printf("Ingrese mes de nacimiento\n");
+	scanf(" %d", &Fecha.Mes);
+	printf("Ingrese año de nacimiento\n");
+	scanf(" %d", &Fecha.Anio);
+  	Fecha.Edad = CalcEdad(Fecha,&flag);
+  	if(flag == 0)
+ 	 	printf("Usted tiene: %d año(s)\n", Fecha.Edad);
+  	else
+  		printf("Usted tiene: %d mes(es)\n", Fecha.Edad);
+}
+
+int CalcEdad(Date Fecha, int *flag)
+{
+	int NoDias1, NoDias2,Diferencia;
 	char Timestamp[200];
+	Date Fecha2;
 	time_t rawtime;
   	struct tm *timeinfo;
-
-	printf("Ingresa tu dia de nacimiento\n");
-	scanf(" %d", &Dia);
-	printf("Ingrese mes de nacimiento\n");
-	scanf(" %d", &Mes);
-	printf("Ingrese año de nacimiento\n");
-	scanf(" %d", &Anio);
-	
-  	time(&rawtime);
+	time(&rawtime);
   	timeinfo = localtime(&rawtime);
   	strftime(Timestamp,200, "%Y,%m,%d", timeinfo);
-  	sscanf(Timestamp,"%d,%d,%d",&AnioActual,&MesActual,&DiaActual);
+  	sscanf(Timestamp,"%d,%d,%d",&Fecha2.Anio,&Fecha2.Mes,&Fecha2.Dia);
 
-  	printf("Nacimiento, Dia: %d, Mes: %d, Anio: %d\n",Dia,Mes,Anio);
-  	printf("Hoy, Dia: %d, Mes: %d, Anio: %d\n",DiaActual,MesActual,AnioActual);
-
-  	Edad(Dia,Mes,Anio,DiaActual,MesActual,AnioActual);
-}
-void Edad(int d, int m, int a,int d2, int m2, int a2)
-{
-	int NoDias1, NoDias2,Diferencia, Edad;
-	NoDias1 = (a*365)+(m*30)+d;
-	NoDias2 = (a2*365)+(m2*30)+d2;
+	NoDias1 = (Fecha.Anio*365)+(Fecha.Mes*30)+Fecha.Dia;
+	NoDias2 = (Fecha2.Anio*365)+(Fecha2.Mes*30)+Fecha2.Dia;
 	Diferencia = NoDias2-NoDias1;
-	Edad = Diferencia / 365;
-	if(Edad <= 0){
-		Edad  = (Diferencia/30);
-		printf("Usted tiene: %d mes(es)\n", Edad);
+	Fecha2.Edad = Diferencia / 365;
+	
+	if(Fecha2.Edad <= 0){
+		Fecha2.Edad  = (Diferencia/30);
+		*flag = 1;
+		return Fecha2.Edad;
 	}
-	else {
-		printf("Usted tiene: %d año(s)\n", Edad);
-	}
+	else 
+		return Fecha2.Edad;
 }
